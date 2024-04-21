fa00a0d8d67cc6f5c62e4c5e2b8291916448613a
schumann <schumann.nicole@edu.htl-villach.at>
2024-04-21 18:30:26 +0200

    slider added (not responsiv)

---
+++ js/slide.js
@@ -0,0 +1,10 @@
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+    const slides = document.getElementsByClassName("mySlides");
+    slides[slideIndex].style.display = "none";
+    slideIndex = (slideIndex + 1) % slides.length;
+    slides[slideIndex].style.display = "block";
+    setTimeout(carousel, 2500);
+}
